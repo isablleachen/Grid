7551f015885f8716727354530ff5bb944a3d3d32
Isabella Chen <xc1338@nyu.edu>
2019-04-09 11:50:00 -0400

    Coding for the board of gems, make player control script

---
+++ GridClone/Assets/Scripts/Board.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject[] gems;
+    public GameObject gemPrefab;
+    public GameObject[,] Allgems;
+    public int rows; 
+    public int columns; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setboard();
+    }
+
+    public void Setboard()
+    {
+        for(int c = 0; c < columns; c++)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                Vector2 tempPosition = new Vector2(c, r);
+                GameObject backgroundTile = Instantiate(gemPrefab,tempPosition,Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "(" + c + "," + r + " ) ";
+                int gemToUse = Random.Range(0, gems.Length);
+                GameObject gem = Instantiate(gems[gemToUse], tempPosition, Quaternion.identity);
+                gem.transform.parent = this.transform;
+                gem.name = "(" + c + "," + r + " ) ";
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GridClone/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public Board board;
+    public GameObject player;
+    public GameObject undergem;
+    public GameObject uppergem;
+    public int columns;
+    public int rows;
+    public int targetX;
+    public int targetY;
+    public int steps = 6;
+    public TextMeshPro step;
+
+    public bool ismatched = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+
+        //GameObject.Find("undergem").transform.position = new Vector2(targetX, targetY);
+        //GameObject.Find("uppergem").transform.position = new Vector2(targetX, targetY += 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step.text = steps.ToString();
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                player.transform.position = new Vector2(targetX, targetY += 1);
+                steps -= 1;
+                //undergem.transform.position = new Vector2(targetX, targetY += 1);
+                //uppergem.transform.position = new Vector2(targetX, targetY);
+        }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                player.transform.position = new Vector2(targetX, targetY -= 1);
+                steps -= 1;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                player.transform.position = new Vector2(targetX -= 1, targetY);
+                steps -= 1;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                player.transform.position = new Vector2(targetX += 1, targetY);
+                steps -= 1;
+            }
+    }
+
+    public void Matches()
+    {
+        
+    }
+}
